b609435ca4e0537c57120b3318c6b74e12a53657
Anton Bortnikov <a.bortnikov@xsolla.com>
2023-02-03 15:55:36 +0500

    counting

--- server.mjs
+++ server.mjs
@@ -1,6 +1,27 @@
-import { createServer } from 'http';
+import express from 'express';
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+const app = express()
+
+let c = {};
+
+const port = process.env.PORT || 8080;
+
+app.get('/2/:id', (req, res) => {
+  if (req.params.id in c) {
+    const v = String(c[req.params.id]);
+    c[req.params.id] = ++c[req.params.id] % 2;
+    res.send(v);
+  } else {
+    c[req.params.id] = 1;
+    res.send('0');
+  }
+});
+
+app.get('/c', (req, res) => {
+  c = {};
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
